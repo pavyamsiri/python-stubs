a52da8b8f56bc905b7137b0c696c80884f9ead8b
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-06-09 19:52:21 +1000

    io/__init__: Clean up imports

--- scipy/io/__init__.pyi
+++ scipy/io/__init__.pyi
@@ -1,21 +1,22 @@
 from . import (
-    arff as arff,
-    harwell_boeing as harwell_boeing,
-    idl as idl,
-    mmio as mmio,
-    netcdf as netcdf,
-    wavfile as wavfile,
+    arff,
+    harwell_boeing,
+    idl,
+    matlab,
+    mmio,
+    netcdf,
+    wavfile,
 )
-from ._fast_matrix_market import mminfo as mminfo, mmread as mmread, mmwrite as mmwrite
+from ._fast_matrix_market import mminfo, mmread, mmwrite
 from ._fortran import (
-    FortranEOFError as FortranEOFError,
-    FortranFile as FortranFile,
-    FortranFormattingError as FortranFormattingError,
+    FortranEOFError,
+    FortranFile,
+    FortranFormattingError,
 )
-from ._harwell_boeing import hb_read as hb_read, hb_write as hb_write
-from ._idl import readsav as readsav
-from ._netcdf import netcdf_file as netcdf_file, netcdf_variable as netcdf_variable
-from .matlab import loadmat as loadmat, savemat as savemat, whosmat as whosmat
+from ._harwell_boeing import hb_read, hb_write
+from ._idl import readsav
+from ._netcdf import netcdf_file, netcdf_variable
+from .matlab import loadmat, savemat, whosmat
 
 __all__ = [
     "FortranEOFError",
@@ -40,6 +41,3 @@ __all__ = [
     "wavfile",
     "whosmat",
 ]
-
-# Names in __all__ with no definition:
-#   matlab
