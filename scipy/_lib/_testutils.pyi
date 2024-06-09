cb4d61735329eeb69d92290a77501521718d097d
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-06-09 17:03:11 +1000

    _lib/_test_utils: Added type stubs

--- scipy/_lib/_testutils.pyi
+++ scipy/_lib/_testutils.pyi
@@ -1,4 +1,10 @@
-from _typeshed import Incomplete
+from collections.abc import Callable, Iterable
+from functools import partial
+from typing import Any, Literal
+
+from typing_extensions import TypeAlias
+
+IntrospectableCallable: TypeAlias = Callable[..., Any]
 
 __all__ = ["PytestTester", "check_free_memory", "_TestPythranFunc", "IS_MUSL"]
 
@@ -7,32 +13,32 @@ IS_MUSL: bool
 class FPUModeChangeWarning(RuntimeWarning): ...
 
 class PytestTester:
-    module_name: Incomplete
-    def __init__(self, module_name) -> None: ...
+    module_name: str
+    def __init__(self, module_name: str) -> None: ...
     def __call__(
         self,
-        label: str = "fast",
-        verbose: int = 1,
-        extra_argv: Incomplete | None = None,
-        doctests: bool = False,
-        coverage: bool = False,
-        tests: Incomplete | None = None,
-        parallel: Incomplete | None = None,
-    ): ...
+        label: Literal["fast", "full"] | str = ...,
+        verbose: int = ...,
+        extra_argv: Iterable[str] | None = ...,
+        doctests: bool = ...,
+        coverage: bool = ...,
+        tests: Iterable[str] | None = ...,
+        parallel: int | None = ...,
+    ) -> bool: ...
 
 class _TestPythranFunc:
-    ALL_INTEGER: Incomplete
-    ALL_FLOAT: Incomplete
-    ALL_COMPLEX: Incomplete
-    arguments: Incomplete
-    partialfunc: Incomplete
-    expected: Incomplete
+    ALL_INTEGER: list[type]
+    ALL_FLOAT: list[type]
+    ALL_COMPLEX: list[type]
+    arguments: dict[int, tuple[Any, Iterable[type]]]
+    partialfunc: partial[Any]
+    expected: Any
     def setup_method(self) -> None: ...
-    def get_optional_args(self, func): ...
-    def get_max_dtype_list_length(self): ...
-    def get_dtype(self, dtype_list, dtype_idx): ...
+    def get_optional_args(self, func: IntrospectableCallable) -> dict[str, Any]: ...
+    def get_max_dtype_list_length(self) -> int: ...
+    def get_dtype(self, dtype_list: Iterable[type], dtype_idx: int) -> type: ...
     def test_all_dtypes(self) -> None: ...
     def test_views(self) -> None: ...
     def test_strided(self) -> None: ...
 
-def check_free_memory(free_mb) -> None: ...
+def check_free_memory(free_mb: float) -> None: ...
